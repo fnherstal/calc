7a04e0d9ecda919f97b80bc6ae912dd40e60c884
fnherstal <fnherstal@naver.com>
2020-06-26 15:20:19 +0900

    이슈(1)template.py

---
+++ template.py
@@ -0,0 +1,16 @@
+html = b"""
+<html>
+    <body>
+        <form action="">
+            a: <input type="number" name="a"><br>
+              b: <input type="number" name="b"><br>
+            <input type="submit">
+        </form>
+        <p>
+	    sum: %(sum)s</br>
+            mul: %(mul)s</br>
+	</p>
+    </body>
+</html>
+"""
+
